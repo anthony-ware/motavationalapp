77cd83fc6af00f8fa15106f637e33dec032ddd3a
anthonyware <anthony.ware10@yahoo.com>
2016-08-23 21:41:50 -0400

    create rules

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
  const express =require('express');
  const app = express();
+ const rules = require('./app/index.js');
+ app.use(rules);
  const port = "8080";
  const host = "0.0.0.0";
  app.listen(port,host);
+
  console.log("Listening on 0.0.0.0:8080...");
