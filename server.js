fbf7cbc212294704148bd8dca09f3bc420eb23d2
anthonyware <anthony.ware10@yahoo.com>
2016-08-23 21:06:40 -0400

    add server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-require('express');
+ const express =require('express');
+ const app = express();
+ const port = "8080";
+ const host = "0.0.0.0";
+ app.listen(port,host);
+ console.log("Listening on 0.0.0.0:8080...");
